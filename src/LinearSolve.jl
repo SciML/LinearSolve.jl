77d52c22f819733b2c2784a05080c6746454a6b8
Jonathan <edelman.jonathan.s@gmail.com>
2021-07-14 15:21:47 -0400

    git push

--- src/LinearSolve.jl
+++ src/LinearSolve.jl
@@ -1,39 +1,104 @@
 module LinearSolve
 
+using Base: cache_dependencies, Bool
 using SciMLBase: AbstractLinearAlgorithm, AbstractDiffEqOperator
+using ArrayInterface: lu_instance
+using UnPack
 using Reexport
 using LinearAlgebra
+using Setfield
 @reexport using SciMLBase
 
 export LUFactorization, QRFactorization, SVDFactorization
 
+#mutable?#
+struct LinearCache{TA,Tb,Tp,Talg,Tc,Tr,Tl}
+    A::TA
+    b::Tb
+    p::Tp
+    alg::Talg
+    cacheval::Tc
+    isfresh::Bool
+    Pr::Tr
+    Pl::Tl
+end
+
+function set_A(cache, A)
+    @set! cache.A = A
+    @set! cache.isfresh = true
+end
+
+function set_b(cache, b)
+    @set! cache.b = b
+end
+
+function set_p(cache, p)
+    @set! cache.p = p
+    # @set! cache.isfresh = true
+end
+
+function SciMLBase.init(prob::LinearProblem, alg; kwargs...)
+    @unpack A, b, p = prob
+    if alg isa LUFactorization
+        fact = lu_instance(A)
+        Tfact = typeof(fact)
+    else
+        fact = nothing
+        Tfact = Any
+    end
+    Pr = nothing
+    Pl = nothing
+    cache = LinearCache{typeof(A),typeof(b),typeof(p),typeof(alg),Tfact,typeof(Pr),typeof(Pl)}(
+        A, b, p, alg, fact, true, Pr, Pl
+    )
+    return cache
+end
+
+SciMLBase.solve!(prob::LinearProblem, alg; kwargs...) = solve!(init(prob, alg; kwargs...))
+SciMLBase.solve!(cache) = solve!(cache, cache.alg)
+
 struct LUFactorization{P} <: AbstractLinearAlgorithm
     pivot::P
 end
+LUFactorization() = LUFactorization(Val(true))
 
-function SciMLBase.solve(prob::LinearProblem, alg::LUFactorization)
-    prob.A isa Union{AbstractMatrix, AbstractDiffEqOperator} || error("LU is not defined for $(typeof(prob.A))")
-    lu(prob.A, alg.pivot) \ prob.b
+function SciMLBase.solve!(cache::LinearCache, alg::LUFactorization)
+    cache.A isa Union{AbstractMatrix, AbstractDiffEqOperator} || error("LU is not defined for $(typeof(prob.A))")
+    if cache.isfresh
+        @set! cache.cacheval = lu!(cache.A, alg.pivot)
+        @set! cache.isfresh = false
+    end
+    ldiv!(cache.cacheval, cache.b)
 end
 
 struct QRFactorization{P} <: AbstractLinearAlgorithm
     pivot::P
     blocksize::Int
 end
+QRFactorization() = QRFactorization(Val(false), 16)
 
-function SciMLBase.solve(prob::LinearProblem, alg::QRFactorization)
-    prob.A isa Union{AbstractMatrix, AbstractDiffEqOperator} || error("QR is not defined for $(typeof(prob.A))")
-    qr(prob.A, alg.pivot; blocksize=alg.blocksize) \ prob.b
+function SciMLBase.solve!(cache::LinearCache, alg::QRFactorization)
+    cache.A isa Union{AbstractMatrix, AbstractDiffEqOperator} || error("QR is not defined for $(typeof(prob.A))")
+    if cache.isfresh
+        @set! cache.cacheval = qr!(cache.A.A, alg.pivot; blocksize=alg.blocksize)
+        @set! cache.isfresh = false
+    end
+    ldiv!(cache.cacheval, cache.b)
 end
 
 struct SVDFactorization{A} <: AbstractLinearAlgorithm
     full::Bool
     alg::A
 end
+SVDFactorization() = SVDFactorization(false, LinearAlgebra.DivideAndConquer())
 
-function SciMLBase.solve(prob::LinearProblem, ::SVDFactorization)
-    prob.A isa Union{AbstractMatrix, AbstractDiffEqOperator} || error("SVD is not defined for $(typeof(prob.A))")
-    svd(prob.A; full=alg.full, alg=alg.alg) \ prob.b
+function SciMLBase.solve!(cache::LinearCache, alg::SVDFactorization)
+    cache.A isa Union{AbstractMatrix, AbstractDiffEqOperator} || error("SVD is not defined for $(typeof(prob.A))")
+    if cache.isfresh
+        @set! cache.cacheval = svd!(cache.A; full=alg.full, alg=alg.alg)
+        @set! cache.isfresh = false
+    end
+    ldiv!(cache.cacheval, cache.b)
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,7 @@ using Test
     A = rand(5, 5)
     b = rand(5)
     prob = LinearProblem(A, b)
-    @test A * solve(prob, LUFactorization(Val(true))) ≈ b
+    @test A * solve!(deepcopy(prob), LUFactorization()) ≈ b
+    @test A * solve!(deepcopy(prob), QRFactorization()) ≈ b
+    @test A * solve!(deepcopy(prob), SVDFactorization()) ≈ b
 end

92038481b3d5d5156cd32b0630e86b0d180c9f23
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2021-07-26 07:17:01 -0400

    Update test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,6 @@ using Test
     b = rand(5)
     prob = LinearProblem(A, b)
     @test A * solve(prob, LUFactorization();alias_A = false, alias_b = false) ≈ b
-    @test A * solve(deepcopy(prob), QRFactorization();alias_A = false, alias_b = false) ≈ b
+    @test A * solve(prob, QRFactorization();alias_A = false, alias_b = false) ≈ b
     @test A * solve(deepcopy(prob), SVDFactorization();alias_A = false, alias_b = false) ≈ b
 end

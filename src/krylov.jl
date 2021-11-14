834f467094146e69ac096660d8eb04d0c4a26100
Vedant Puri <vedantpuri@gmail.com>
2021-11-14 11:04:44 -0500

    krylovJL should take preconditioner, etc from cache

--- src/krylov.jl
+++ src/krylov.jl
@@ -10,10 +10,10 @@ function KrylovJL(A, b, args...; solver = gmres, kwargs...)
     return KrylovJL(solver, A, b, args, kwargs)
 end
 
-function SciMLBase.solve(prob::LinearProblem, alg::KrylovJL, args...; kwargs...)
-    @unpack A, b, p = prob
+function SciMLBase.solve(cache::LinearCache, alg::KrylovJL,args...;kwargs...)
+    @unpack A, b = cache
     x, stats = alg.solver(A, b, args...; kwargs...)
     resid = A * x - b
     retcode = stats.solved ? :Success : :Failure
-    return SciMLBase.build_solution(prob, alg, x, resid; retcode = retcode)
+    return x #SciMLBase.build_solution(prob, alg, x, resid; retcode = retcode)
 end

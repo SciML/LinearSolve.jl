edac50ce9c5745f2e4f1b4bb5578b1a8425050ae
Vedant Puri <vedantpuri@gmail.com>
2021-11-14 11:01:20 -0500

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,18 +2,22 @@ using LinearSolve
 using Test
 
 @testset "LinearSolve.jl" begin
-    A = rand(5, 5)
-    b = rand(5)
+    n = 10
+    A = rand(n, n)
+    b = rand(n)
     prob = LinearProblem(A, b)
 
+    x = rand(n)
+
     # Factorization
-    @test A * solve(prob, LUFactorization(); alias_A = false, alias_b = false) ≈
-          b
-    @test A * solve(prob, QRFactorization(); alias_A = false, alias_b = false) ≈
-          b
-    @test A *
-          solve(prob, SVDFactorization(); alias_A = false, alias_b = false) ≈ b
+    @test A * solve(prob, LUFactorization();)  ≈ b
+    @test A * solve(prob, QRFactorization();)  ≈ b
+    @test A * solve(prob, SVDFactorization();) ≈ b
 
     # Krylov
     @test A * solve(prob, KrylovJL(A, b)) ≈ b
+
+    # make algorithm callable - interoperable with DiffEq ecosystem
+    @test A * LUFactorization()(x,A,b) ≈ b 
+    @test_broken A * x ≈ b # in place
 end

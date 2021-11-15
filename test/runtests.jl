61ea0ebed62cba5d1f465c4f38e7dad851023681
Vedant Puri <vedantpuri@gmail.com>
2021-11-14 21:01:03 -0500

    a reasonable test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,26 @@ using LinearSolve
 using Test
 
 @testset "LinearSolve.jl" begin
-    n = 10
-    A = rand(n, n)
-    b = rand(n)
+    using LinearAlgebra
+    n = 100
+    x = Array(range(start=-1,stop=1,length=n))
+    uu= @. sin(pi*x)
+
+    dx = 2/(n-1)
+
+    AA = Tridiagonal(ones(n-1),-2ones(n),ones(n-1))/(dx*dx)
+    bb = @. -(pi^2)*uu
+    id = Matrix(I,n,n)
+    R  = id[2:end-1,:]
+
+    A = R * AA * R'
+    u = R * uu
+    b = A * u #R * bb
+
+    @test isapprox(A*u,b;atol=1e-2)
     prob = LinearProblem(A, b)
 
-    x = rand(n)
+    x = zero(b)
 
     # Factorization
     @test A * solve(prob, LUFactorization();)  ≈ b
@@ -18,10 +32,21 @@ using Test
     @test A * solve(prob, KrylovJL(A, b)) ≈ b
 
     # make algorithm callable - interoperable with DiffEq ecosystem
-    @test A * LUFactorization()(x,A,b) ≈ b 
-    @test A * KrylovJL()(x,A,b) ≈ b 
+    @test A * LUFactorization()(x,A,b)  ≈ b 
+    @test A * QRFactorization()(x,A,b)  ≈ b 
+    @test A * SVDFactorization()(x,A,b) ≈ b 
+    @test A * KrylovJL()(x,A,b)         ≈ b 
 
     # in place
-    KrylovJL()(x,A,b)
+    LUFactorization()(x,A,b) 
     @test A * x ≈ b
+    QRFactorization()(x,A,b) 
+    @test A * x ≈ b
+    SVDFactorization()(x,A,b)
+    @test A * x ≈ b
+    KrylovJL()(x,A,b)        
+    @test A * x ≈ b
+
+    # test on some ODEProblem
+#   using OrdinaryDiffEq
 end

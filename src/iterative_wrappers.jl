a886bcae7402a279e09f772f60c1c6b7a6d9210f
Vedant Puri <vedantpuri@gmail.com>
2022-02-22 23:07:59 -0500

    initital commit

--- src/iterative_wrappers.jl
+++ src/iterative_wrappers.jl
@@ -231,3 +231,40 @@ function purge_history!(iter::IterativeSolvers.GMRESIterable, x, b)
   iter.β = iter.residual.current
   nothing
 end
+
+## KrylovKit.jl
+
+struct KrylovKitJL{F,I,K} <: AbstractKrylovSubspaceMethod
+    KrylovAlg::F
+    gmres_restart::I
+    kwargs::K
+end
+
+function KrylovKitJL(KrylovAlg = KrylovKit.GMRES, gmres_restart=0, kwargs...)
+    return KrylovJL(KrylovAlg, gmres_restart, kwargs)
+end
+
+KrylovKitJL_GMRES(kwargs...) = KrylovKitJL()
+KrylovKitJL_CG(kwargs...) = KrylovKitJL(KrylovKitAlg=KrylovKit.CG, kwargs...)
+
+function SciMLBase.solve(cache::LinearCache, alg::KrylovKitJL, kwargs...)
+
+    atol      = float(cache.abstol)
+    rtol      = float(cache.reltol)
+    maxiter   = cache.maxiters
+    verbosity = cache.verbose ? 1 : 0
+    krylovdim = alg.gmres_restart
+
+    kwargs = (atol=atol, rtol=rtol, maxiter=maxiter, verbosity=verbosity,
+              krylovdim = krylovdim, alg.kwargs...)
+
+    x, info = KrylovKit.linsolve(cache.A, cache.b, cache.u, alg.KrylovAlg,
+                                 [a₀::Number = 0, a₁::Number = 1])
+    copy!(cache.u, x)
+    resid = info.normres
+    retcode = info.converged == 1 ? :Default : :DidNotConverge
+    iters = info.numiter
+
+    return SciMLBase.build_linear_solution(alg,cache.u, resid,cache;
+                                           retcode = retcode, iters = iters)
+end

340e67d43502199211ef5632ba1e858de2dd37fb
Vedant Puri <vedantpuri@gmail.com>
2022-04-01 21:57:48 -0400

    ldiv

---
+++ src/function_call.jl
@@ -0,0 +1,26 @@
+struct FunctionCall{F,A} <: SciMLLinearSolveAlgorithm
+    func!::F
+    argsyms::A
+
+    function FunctionCall(func!::Function, argsyms::Tuple)
+        new{typeof(func!), typeof(argsyms)}(func!, argsyms)
+    end
+end
+
+LdivBang2Args() = FunctionCall(ldiv!, (:A, :u))
+LdivBang3Args() = FunctionCall(ldiv!, (:u, :A, :b))
+
+function (f::FunctionCall)(cache::LinearCache)
+    @unpack func!, argsyms = f
+    args = [getproperty(cache,argsym) for argsym in argsyms]
+    func!(args...)
+end
+
+function SciMLBase.solve(cache::LinearCache, alg::FunctionCall,
+                         args...; kwargs...)
+    @unpack u, b = cache
+    copy!(u, b)
+    alg(cache)
+
+    return SciMLBase.build_linear_solution(alg,cache.u,nothing,cache)
+end

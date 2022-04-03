1f7a20b0377a35cd1f01e64dc4162aa165ba17d7
Vedant Puri <vedantpuri@gmail.com>
2022-04-03 14:25:00 -0400

    test cases

--- src/function_call.jl
+++ src/function_call.jl
@@ -1,26 +1,46 @@
+
+""" user passes in inverse function, and arg symbols """
 struct FunctionCall{F,A} <: SciMLLinearSolveAlgorithm
     func!::F
-    argsyms::A
+    argnames::A
 
-    function FunctionCall(func!::Function, argsyms::Tuple)
-        new{typeof(func!), typeof(argsyms)}(func!, argsyms)
+    function FunctionCall(func!::Function, argnames::Tuple)
+#       @assert hasfield(::LinearCache, argnames)
+#       @assert isdefined
+        new{typeof(func!), typeof(argnames)}(func!, argnames)
     end
 end
 
-LdivBang2Args() = FunctionCall(ldiv!, (:A, :u))
-LdivBang3Args() = FunctionCall(ldiv!, (:u, :A, :b))
-
 function (f::FunctionCall)(cache::LinearCache)
-    @unpack func!, argsyms = f
-    args = [getproperty(cache,argsym) for argsym in argsyms]
+    @unpack func!, argnames = f
+    args = [getproperty(cache,argname) for argname in argnames]
     func!(args...)
 end
 
 function SciMLBase.solve(cache::LinearCache, alg::FunctionCall,
                          args...; kwargs...)
     @unpack u, b = cache
-    copy!(u, b)
     alg(cache)
+    return SciMLBase.build_linear_solution(alg,cache.u,nothing,cache)
+end
 
+##
+
+""" apply ldiv!(A, u) """
+struct ApplyLDivBang2Args <: SciMLLinearSolveAlgorithm end
+function SciMLBase.solve(cache::LinearCache, alg::ApplyLDivBang2Args,
+                         args...; kwargs...)
+    @unpack A, b, u = cache
+    copy!(u, b)
+    LinearAlgebra.ldiv!(A, u)
+    return SciMLBase.build_linear_solution(alg,cache.u,nothing,cache)
+end
+
+""" apply ldiv!(u, A, b) """
+struct ApplyLDivBang3Args <: SciMLLinearSolveAlgorithm end
+function SciMLBase.solve(cache::LinearCache, alg::ApplyLDivBang3Args,
+                         args...; kwargs...)
+    @unpack A, b, u = cache
+    LinearAlgebra.ldiv!(u, A, b)
     return SciMLBase.build_linear_solution(alg,cache.u,nothing,cache)
 end

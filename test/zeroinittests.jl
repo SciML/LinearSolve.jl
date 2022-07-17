2d8678da9332bbf638e9bf26f7cb52fd2f0059d8
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-07-17 11:23:23 -0400

    Capture KLU old failure in the test

--- test/zeroinittests.jl
+++ test/zeroinittests.jl
@@ -7,14 +7,12 @@ Anz = deepcopy(A)
 A.nzval .= 0
 cache_kwargs = (; verbose = true, abstol = 1e-8, reltol = 1e-8, maxiter = 30)
 
-
 function test_nonzero_init(alg=nothing)
     linprob = LinearProblem(A,b)
 
-    cache = init(linprob)
+    cache = init(linprob,alg)
     cache = LinearSolve.set_A(cache, Anz)
-    @show cache.A
-    sol = solve(cache,nothing;cache_kwargs...)
+    sol = solve(cache;cache_kwargs...)
     @test sol.u == b
 end
 

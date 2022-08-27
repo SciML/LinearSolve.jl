ec2dfad21b6ef35a4fa28ca10c595c84a96569a3
Christopher Rackauckas <accounts@chrisrackauckas.com>
2022-08-27 08:03:09 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -9,6 +9,14 @@ makedocs(sitename = "LinearSolve.jl",
          authors = "Chris Rackauckas",
          modules = [LinearSolve, LinearSolve.SciMLBase],
          clean = true, doctest = false,
+         strict=[
+                 :doctest, 
+                 :linkcheck, 
+                 :parse_error,
+                 :example_block,
+                 # Other available options are
+                 # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
+         ],
          format = Documenter.HTML(analytics = "UA-90474609-3",
                                   assets = ["assets/favicon.ico"],
                                   canonical = "https://linearsolve.sciml.ai/stable/"),

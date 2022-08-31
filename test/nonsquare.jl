79f2a0ca34a7085a36bfd3fafd135bf478cb41e1
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-08-31 14:15:48 -0400

    fixes

--- test/nonsquare.jl
+++ test/nonsquare.jl
@@ -3,17 +3,18 @@ using SparseArrays, LinearAlgebra
 
 m, n = 13, 3
 
-A = rand(m, n); b = rand(m);
+A = rand(m, n);
+b = rand(m);
 prob = LinearProblem(A, b)
-res = A\b
+res = A \ b
 @test solve(prob).u ≈ res
 @test solve(prob, QRFactorization()) ≈ res
 @test solve(prob, KrylovJL_LSMR()) ≈ res
 
-A = sprand(m, n, 0.5); b = rand(m);
+A = sprand(m, n, 0.5);
+b = rand(m);
 prob = LinearProblem(A, b)
-res = A\b
+res = A \ b
 @test solve(prob).u ≈ res
 @test solve(prob, QRFactorization()) ≈ res
 @test solve(prob, KrylovJL_LSMR()) ≈ res
-

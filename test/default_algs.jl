a54cbe5804d627715963f2babdb627b3bba098a2
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-01-24 22:54:12 -0500

    namespace?

--- test/default_algs.jl
+++ test/default_algs.jl
@@ -1,10 +1,10 @@
-using LinearSolve, SparseArrays
-LinearSolve.defaultalg(nothing, zeros(3)) isa GenericLUFactorization
-LinearSolve.defaultalg(nothing, zeros(50)) isa RFLUFactorization
-LinearSolve.defaultalg(nothing, zeros(600)) isa LUFactorization
-LinearSolve.defaultalg(Diagonal(zeros(5)), zeros(5)) isa DiagonalFactorization
+using LinearSolve, LinearAlgebra, SparseArrays, Test
+@test LinearSolve.defaultalg(nothing, zeros(3)) isa GenericLUFactorization
+@test LinearSolve.defaultalg(nothing, zeros(50)) isa RFLUFactorization
+@test LinearSolve.defaultalg(nothing, zeros(600)) isa LUFactorization
+@test LinearSolve.defaultalg(LinearAlgebra.Diagonal(zeros(5)), zeros(5)) isa DiagonalFactorization
 
-LinearSolve.defaultalg(nothing, zeros(5), OperatorAssumptions{false}) isa QRFactorization
+@test LinearSolve.defaultalg(nothing, zeros(5), LinearSolve.OperatorAssumptions{false}()) isa QRFactorization
 
-LinearSolve.defaultalg(sprand(1000,1000,0.01), zeros(1000)) isa KLUFactorization
-LinearSolve.defaultalg(sprand(11000,11000,0.001), zeros(11000)) isa UMFPACKFactorization
+@test LinearSolve.defaultalg(sprand(1000,1000,0.01), zeros(1000)) isa KLUFactorization
+@test LinearSolve.defaultalg(sprand(11000,11000,0.001), zeros(11000)) isa UMFPACKFactorization

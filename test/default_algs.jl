bf6b60e58e0095c921fe66fd82df9efa7f9ab52c
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-01-25 05:39:17 -0500

    format

--- test/default_algs.jl
+++ test/default_algs.jl
@@ -2,9 +2,12 @@ using LinearSolve, LinearAlgebra, SparseArrays, Test
 @test LinearSolve.defaultalg(nothing, zeros(3)) isa GenericLUFactorization
 @test LinearSolve.defaultalg(nothing, zeros(50)) isa RFLUFactorization
 @test LinearSolve.defaultalg(nothing, zeros(600)) isa LUFactorization
-@test LinearSolve.defaultalg(LinearAlgebra.Diagonal(zeros(5)), zeros(5)) isa DiagonalFactorization
+@test LinearSolve.defaultalg(LinearAlgebra.Diagonal(zeros(5)), zeros(5)) isa
+      DiagonalFactorization
 
-@test LinearSolve.defaultalg(nothing, zeros(5), LinearSolve.OperatorAssumptions{false}()) isa QRFactorization
+@test LinearSolve.defaultalg(nothing, zeros(5),
+                             LinearSolve.OperatorAssumptions{false}()) isa QRFactorization
 
-@test LinearSolve.defaultalg(sprand(1000,1000,0.01), zeros(1000)) isa KLUFactorization
-@test LinearSolve.defaultalg(sprand(11000,11000,0.001), zeros(11000)) isa UMFPACKFactorization
+@test LinearSolve.defaultalg(sprand(1000, 1000, 0.01), zeros(1000)) isa KLUFactorization
+@test LinearSolve.defaultalg(sprand(11000, 11000, 0.001), zeros(11000)) isa
+      UMFPACKFactorization

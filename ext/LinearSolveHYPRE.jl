50ae7351875a3070613b502de03e8b06cbf35876
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-03-24 19:20:42 -0400

    avoid no size on HYPREVector

--- ext/LinearSolveHYPRE.jl
+++ ext/LinearSolveHYPRE.jl
@@ -201,7 +201,9 @@ function SciMLBase.solve(cache::LinearCache, alg::HYPREAlgorithm, args...; kwarg
     iters = Int(HYPRE.GetNumIterations(hcache.solver))
     retc = SciMLBase.ReturnCode.Default # TODO: Fetch from solver
 
-    ret = SciMLBase.build_linear_solution(alg, cache.u, resid, cache; iters)
+    ret = SciMLBase.LinearSolution{T, N, typeof(cache.u), typeof(resid), typeof(alg),
+                                   typeof(cache)}(cache.u, resid, alg, retc, iters, cache)
+
     return ret
 end
 

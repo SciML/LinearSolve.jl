3785ed9689d74abf946e6e4c11a897c1cdea9946
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-05-17 12:41:51 -0400

    setup warnings to only call once

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,8 +1,17 @@
+const warned_a = Ref(false)
+const warned_b = Ref(false)
+const warned_u = Ref(false)
+const warned_p = Ref(false)
+const warned_prec = Ref(false)
+const warned_cacheval = Ref(false)
 """
 $(SIGNATURES)
 """
 function set_A(cache::LinearCache, A)
-    @warn "set_A is deprecated for mutation on the cache. Use `cache.A = A"
+    if !warned_a[]
+        @warn "set_A is deprecated for mutation on the cache. Use `cache.A = A"
+        warned_a[] = true
+    end
     @set! cache.A = A
     @set! cache.isfresh = true
     return cache
@@ -12,7 +21,10 @@ end
 $(SIGNATURES)
 """
 function set_b(cache::LinearCache, b)
-    @warn "set_b is deprecated for mutation on the cache. Use `cache.b = b"
+    if !warned_b[]
+        @warn "set_b is deprecated for mutation on the cache. Use `cache.b = b"
+        warned_b[] = true
+    end
     @set! cache.b = b
     return cache
 end
@@ -21,7 +33,10 @@ end
 $(SIGNATURES)
 """
 function set_u(cache::LinearCache, u)
-    @warn "set_u is deprecated for mutation on the cache. Use `cache.u = u"
+    if !warned_u[]
+        @warn "set_u is deprecated for mutation on the cache. Use `cache.u = u"
+        warned_u[] = true
+    end
     @set! cache.u = u
     return cache
 end
@@ -30,7 +45,10 @@ end
 $(SIGNATURES)
 """
 function set_p(cache::LinearCache, p)
-    @warn "set_p is deprecated for mutation on the cache. Use `cache.p = p"
+    if !warned_p[]
+        @warn "set_p is deprecated for mutation on the cache. Use `cache.p = p"
+        warned_p[] = true
+    end
     @set! cache.p = p
     #   @set! cache.isfresh = true
     return cache
@@ -40,14 +58,20 @@ end
 $(SIGNATURES)
 """
 function set_prec(cache, Pl, Pr)
-    @warn "set_prec is deprecated for mutation on the cache. Use `cache.Pl = Pl; cache.Pr = Pr"
+    if !warned_prec[]
+        @warn "set_prec is deprecated for mutation on the cache. Use `cache.Pl = Pl; cache.Pr = Pr"
+        warned_prec[] = true
+    end
     @set! cache.Pl = Pl
     @set! cache.Pr = Pr
     return cache
 end
 
 function set_cacheval(cache::LinearCache, alg_cache)
-    @warn "set_cacheval is deprecated for mutation on the cache. Use `cache.cacheval = cacheval; cache.isfresh = false"
+    if !warned_cacheval[]
+        @warn "set_cacheval is deprecated for mutation on the cache. Use `cache.cacheval = cacheval; cache.isfresh = false"
+        warned_cacheval[] = true
+    end
     if cache.isfresh
         @set! cache.cacheval = alg_cache
         @set! cache.isfresh = false
@@ -56,11 +80,9 @@ function set_cacheval(cache::LinearCache, alg_cache)
 end
 
 @static if VERSION >= v"1.7"
-    @deprecate SciMLBase.solve(cache::LinearCache, args...; kwargs...) SciMLBase.solve!(cache::LinearCache,
-                                                                                        args...;
-                                                                                        kwargs...) false
+    @deprecate SciMLBase.solve(cache::LinearCache, args...; kwargs...) SciMLBase.solve!(cache::LinearCache, args...; kwargs...) false
 else
-    function SciMLBase.solve(cache::LinearCache, args...; kwargs...)
+    function SciMLBase.solve(cache::LinearCache, args...; kwargs...) 
         @warn "SciMLBase.solve(cache::LinearCache, args...; kwargs...) is deprecated for SciMLBase.solve!(cache::LinearCache, args...; kwargs...) to follow the CommonSolve.jl interface."
         SciMLBase.solve!(cache::LinearCache, args...; kwargs...)
     end

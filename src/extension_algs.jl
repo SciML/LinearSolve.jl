c722462747c12890c18aa114fa513cd1c372c2fc
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-05-29 10:29:49 -0700

    format

--- src/extension_algs.jl
+++ src/extension_algs.jl
@@ -16,7 +16,7 @@ struct HYPREAlgorithm <: SciMLLinearSolveAlgorithm
     end
 end
 
-struct CudaOffloadFactorization <: LinearSolve.AbstractFactorization 
+struct CudaOffloadFactorization <: LinearSolve.AbstractFactorization
     @static if VERSION >= v"1.9-"
         function CudaOffloadFactorization()
             ext = Base.get_extension(@__MODULE__, :LinearSolveCUDAExt)
@@ -33,19 +33,18 @@ MKLPardisoFactorize(; kwargs...) = PardisoJL(; solver_type = 0, kwargs...)
 MKLPardisoIterate(; kwargs...) = PardisoJL(; solver_type = 1, kwargs...)
 
 @static if VERSION >= v"1.9-"
-    struct PardisoJL{T1,T2} <: LinearSolve.SciMLLinearSolveAlgorithm
+    struct PardisoJL{T1, T2} <: LinearSolve.SciMLLinearSolveAlgorithm
         nprocs::Union{Int, Nothing}
         solver_type::T1
         matrix_type::T2
         iparm::Union{Vector{Tuple{Int, Int}}, Nothing}
         dparm::Union{Vector{Tuple{Int, Int}}, Nothing}
 
-        function PardisoJL(;nprocs::Union{Int, Nothing} = nothing,
-            solver_type = nothing,
-            matrix_type = nothing,
-            iparm::Union{Vector{Tuple{Int, Int}}, Nothing} = nothing,
-            dparm::Union{Vector{Tuple{Int, Int}}, Nothing} = nothing)
-
+        function PardisoJL(; nprocs::Union{Int, Nothing} = nothing,
+                           solver_type = nothing,
+                           matrix_type = nothing,
+                           iparm::Union{Vector{Tuple{Int, Int}}, Nothing} = nothing,
+                           dparm::Union{Vector{Tuple{Int, Int}}, Nothing} = nothing)
             ext = Base.get_extension(@__MODULE__, :LinearSolvePardisoExt)
             if ext === nothing
                 error("PardisoJL requires that Pardiso is loaded, i.e. `using Pardiso`")
@@ -65,5 +64,5 @@ else
         matrix_type::Any = nothing
         iparm::Union{Vector{Tuple{Int, Int}}, Nothing} = nothing
         dparm::Union{Vector{Tuple{Int, Int}}, Nothing} = nothing
-    end    
+    end
 end

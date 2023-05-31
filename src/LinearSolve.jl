7b2d67557925e185238bb016c01cbf98543c6f09
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-05-31 15:01:32 -0700

    backwards compatibility

--- src/LinearSolve.jl
+++ src/LinearSolve.jl
@@ -68,6 +68,13 @@ include("deprecated.jl")
     include("factorization_sparse.jl")
 end
 
+@static if !isdefined(Base, :get_extension)
+    function __init__()
+        @require IterativeSolvers="b77e0a4c-d291-57a0-90e8-8db25a27a240" begin include("../ext/LinearSolveIterativeSolversExt.jl") end
+        @require KrylovKit="0b1a1467-8014-51b9-945f-bf0ae24f4b77" begin include("../ext/LinearSolveKrylovKit.jl") end
+    end
+end
+
 const IS_OPENBLAS = Ref(true)
 isopenblas() = IS_OPENBLAS[]
 

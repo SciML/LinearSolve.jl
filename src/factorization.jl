10758e807972d539515784bb9ce9e5f4426aec18
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-06-18 10:18:53 -0400

    Improve a few docstrings

--- src/factorization.jl
+++ src/factorization.jl
@@ -30,13 +30,13 @@ end
 
 Julia's built in `lu`. Equivalent to calling `lu!(A)`
     
-      * On dense matrices, this uses the current BLAS implementation of the user's computer,
-        which by default is OpenBLAS but will use MKL if the user does `using MKL` in their
-        system.
-      * On sparse matrices, this will use UMFPACK from SuiteSparse. Note that this will not
-        cache the symbolic factorization.
-      * On CuMatrix, it will use a CUDA-accelerated LU from CuSolver.
-      * On BandedMatrix and BlockBandedMatrix, it will use a banded LU.
+* On dense matrices, this uses the current BLAS implementation of the user's computer,
+which by default is OpenBLAS but will use MKL if the user does `using MKL` in their
+system.
+* On sparse matrices, this will use UMFPACK from SuiteSparse. Note that this will not
+cache the symbolic factorization.
+* On CuMatrix, it will use a CUDA-accelerated LU from CuSolver.
+* On BandedMatrix and BlockBandedMatrix, it will use a banded LU.
 
 ## Positional Arguments
 
@@ -136,12 +136,12 @@ end
 
 Julia's built in `qr`. Equivalent to calling `qr!(A)`.
     
-      * On dense matrices, this uses the current BLAS implementation of the user's computer
-        which by default is OpenBLAS but will use MKL if the user does `using MKL` in their
-        system.
-      * On sparse matrices, this will use SPQR from SuiteSparse
-      * On CuMatrix, it will use a CUDA-accelerated QR from CuSolver.
-      * On BandedMatrix and BlockBandedMatrix, it will use a banded QR.
+* On dense matrices, this uses the current BLAS implementation of the user's computer
+which by default is OpenBLAS but will use MKL if the user does `using MKL` in their
+system.
+* On sparse matrices, this will use SPQR from SuiteSparse
+* On CuMatrix, it will use a CUDA-accelerated QR from CuSolver.
+* On BandedMatrix and BlockBandedMatrix, it will use a banded QR.
 """
 struct QRFactorization{P} <: AbstractFactorization
     pivot::P
@@ -324,9 +324,9 @@ end
 
 Julia's built in `svd`. Equivalent to `svd!(A)`.
     
-      * On dense matrices, this uses the current BLAS implementation of the user's computer
-        which by default is OpenBLAS but will use MKL if the user does `using MKL` in their
-        system.
+* On dense matrices, this uses the current BLAS implementation of the user's computer
+which by default is OpenBLAS but will use MKL if the user does `using MKL` in their
+system.
 """
 struct SVDFactorization{A} <: AbstractFactorization
     full::Bool

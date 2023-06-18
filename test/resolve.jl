7f5b1808fd0a78179522b842feada700721278b9
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-06-18 12:23:39 -0400

    fix test definition for symmetric

--- test/resolve.jl
+++ test/resolve.jl
@@ -53,8 +53,9 @@ A = Symmetric([1.0 2.0
 linsolve.A = A
 @test solve!(linsolve).u ≈ [1.0, 0.0]
 
-A = Symmetric([1.0 2.0
-    2.0 1.0])
+A = [1.0 2.0
+2.0 1.0]
+A = Symmetric(A * A')
 b = [1.0, 2.0]
 prob = LinearProblem(A, b)
 linsolve = init(prob, CholeskyFactorization(), alias_A = false, alias_b = false)

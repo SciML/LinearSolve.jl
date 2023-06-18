e41b85baaa2daf80b996c774323d962aa1018147
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-06-18 12:45:27 -0400

    Fix symmetric test

--- test/resolve.jl
+++ test/resolve.jl
@@ -59,9 +59,10 @@ A = Symmetric(A * A')
 b = [1.0, 2.0]
 prob = LinearProblem(A, b)
 linsolve = init(prob, CholeskyFactorization(), alias_A = false, alias_b = false)
-@test solve!(linsolve).u ≈ [1.0, 0.0]
-@test solve!(linsolve).u ≈ [1.0, 0.0]
-A = Symmetric([1.0 2.0
-    2.0 1.0])
+@test solve!(linsolve).u ≈ [-1/3, 2/3]
+@test solve!(linsolve).u ≈ [-1/3, 2/3]
+A = [1.0 2.0
+2.0 1.0]
+A = Symmetric(A * A')
 b = [1.0, 2.0]
-@test solve!(linsolve).u ≈ [1.0, 0.0]
+@test solve!(linsolve).u ≈ [-1/3, 2/3]

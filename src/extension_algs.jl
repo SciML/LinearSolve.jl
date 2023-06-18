9c85e96694fcc6779c98eb6d6476b3d8fb28f6bc
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-06-18 10:19:28 -0400

    don't forget pardiso

--- src/extension_algs.jl
+++ src/extension_algs.jl
@@ -180,6 +180,28 @@ MKLPardisoIterate(; kwargs...) = PardisoJL(; solver_type = 1, kwargs...)
         end
     end
 else
+    """
+    ```julia
+    PardisoJL(; nprocs::Union{Int, Nothing} = nothing,
+                solver_type = nothing,
+                matrix_type = nothing,
+                iparm::Union{Vector{Tuple{Int, Int}}, Nothing} = nothing,
+                dparm::Union{Vector{Tuple{Int, Int}}, Nothing} = nothing)
+    ```
+
+    A generic method using MKL Pardiso. Specifying `solver_type` is required.
+
+    !!! note
+
+        Using this solver requires adding the package Pardiso.jl, i.e. `using Pardiso`
+
+    ## Keyword Arguments
+
+    For the definition of the keyword arguments, see the Pardiso.jl documentation.
+    All values default to `nothing` and the solver internally determines the values
+    given the input types, and these keyword arguments are only for overriding the
+    default handling process. This should not be required by most users.
+    """
     Base.@kwdef struct PardisoJL <: LinearSolve.SciMLLinearSolveAlgorithm
         nprocs::Union{Int, Nothing} = nothing
         solver_type::Any = nothing

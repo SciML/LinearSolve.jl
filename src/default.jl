e622fa495f238e96e758aac74828c20aa02a966d
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-07-16 19:35:36 -0400

    Hotfix printing

--- src/default.jl
+++ src/default.jl
@@ -79,8 +79,6 @@ end
     function defaultalg(A::AbstractSparseMatrixCSC{<:Union{Float64, ComplexF64}, Ti}, b,
         assump::OperatorAssumptions) where {Ti}
         if assump.issq
-            @show length(b) <= 10_000
-            @show length(nonzeros(A)) / length(A)
             if length(b) <= 10_000 && length(nonzeros(A)) / length(A) < 2e-4
                 DefaultLinearSolver(DefaultAlgorithmChoice.KLUFactorization)
             else

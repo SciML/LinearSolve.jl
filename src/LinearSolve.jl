f077588b9d7b2cb9d289f371ce7f25b50e3c2bce
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-08-08 12:20:53 -0400

    Consolidate init

--- src/LinearSolve.jl
+++ src/LinearSolve.jl
@@ -121,20 +121,6 @@ end
     include("factorization_sparse.jl")
 end
 
-@static if !isdefined(Base, :get_extension)
-    function __init__()
-        @require IterativeSolvers="b77e0a4c-d291-57a0-90e8-8db25a27a240" begin
-            include("../ext/LinearSolveIterativeSolversExt.jl")
-        end
-        @require KrylovKit="0b1a1467-8014-51b9-945f-bf0ae24f4b77" begin
-            include("../ext/LinearSolveKrylovKitExt.jl")
-        end
-        @require MKL_jll="856f044c-d86e-5d09-b602-aeab76dc8ba7" begin
-            include("../ext/LinearSolveMKLExt.jl")
-        end
-    end
-end
-
 const IS_OPENBLAS = Ref(true)
 isopenblas() = IS_OPENBLAS[]
 
--- src/init.jl
+++ src/init.jl
@@ -5,4 +5,15 @@ function __init__()
     else
         IS_OPENBLAS[] = occursin("openblas", BLAS.get_config().loaded_libs[1].libname)
     end
+    @static if !isdefined(Base, :get_extension)
+        @require IterativeSolvers="b77e0a4c-d291-57a0-90e8-8db25a27a240" begin
+            include("../ext/LinearSolveIterativeSolversExt.jl")
+        end
+        @require KrylovKit="0b1a1467-8014-51b9-945f-bf0ae24f4b77" begin
+            include("../ext/LinearSolveKrylovKitExt.jl")
+        end
+        @require MKL_jll="856f044c-d86e-5d09-b602-aeab76dc8ba7" begin
+            include("../ext/LinearSolveMKLExt.jl")
+        end
+    end
 end

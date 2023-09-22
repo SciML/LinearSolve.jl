84c5196d9c49ada2ac69f34e702300d7ce1d498e
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-09-22 19:40:44 -0400

    add some batch tests

--- test/enzyme.jl
+++ test/enzyme.jl
@@ -28,3 +28,17 @@ db12 = FiniteDiff.finite_difference_gradient(x->f(A,x, b2), copy(b1))
 @test dA ≈ dA2
 @test db1 ≈ db12
 @test db2 == zeros(4)
+
+A = rand(n, n);
+dA = zeros(n, n);
+dA2 = zeros(n, n);
+b1 = rand(n);
+db1 = zeros(n);
+db12 = zeros(n);
+
+b2 = rand(n);
+db2 = zeros(n);
+db22 = zeros(n);
+
+@test_broken Enzyme.autodiff(Reverse, f, Duplicated(copy(A), dA), BatchDuplicated(copy(b1), (db1, db12)), BatchDuplicated(copy(b2), (db2, db22)))
+@test_broken Enzyme.autodiff(Reverse, f, BatchDuplicated(copy(A), (dA, dA2)), Duplicated(copy(b1), db1), Duplicated(copy(b2), db2))

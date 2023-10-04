9ea818d5fa6313b092ce3d006f46e349aa1b9a61
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-10-04 21:57:21 +0200

    simplify gpu code

--- ext/LinearSolveCUDAExt.jl
+++ ext/LinearSolveCUDAExt.jl
@@ -17,13 +17,6 @@ function SciMLBase.solve!(cache::LinearSolve.LinearCache, alg::CudaOffloadFactor
 end
 
 function LinearSolve.do_factorization(alg::CudaOffloadFactorization, A, b, u)
-    A isa Union{AbstractMatrix, AbstractSciMLOperator} ||
-        error("LU is not defined for $(typeof(A))")
-
-    if A isa Union{MatrixOperator, DiffEqArrayOperator}
-        A = A.A
-    end
-
     fact = lu(CUDA.CuArray(A))
     return fact
 end

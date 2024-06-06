8ac858d2c3128f5947f2b7784f9dd4fe3ec06e94
termi-official <termi-official@users.noreply.github.com>
2024-06-07 01:08:16 +0200

    Fix returncodes and stats for iterative solvers and add test coverage.

---
+++ test/retcodes.jl
@@ -0,0 +1,46 @@
+@testset "Return codes" begin
+    alglist = (
+        LUFactorization,
+        QRFactorization,
+        DiagonalFactorization,
+        DirectLdiv!,
+        SparspakFactorization,
+        KLUFactorization,
+        UMFPACKFactorization,
+        KrylovJL_GMRES,
+        GenericLUFactorization,
+        RFLUFactorization,
+        LDLtFactorization,
+        BunchKaufmanFactorization,
+        CHOLMODFactorization,
+        SVDFactorization,
+        CholeskyFactorization,
+        NormalCholeskyFactorization,
+        AppleAccelerateLUFactorization,
+        MKLLUFactorization,
+        KrylovJL_CRAIGMR,
+        KrylovJL_LSMR,
+    )
+
+    @testset "Success" begin
+        for alg in alglist
+            A = [2.0 1.0; -1.0 1.0]
+            b = [-1.0, 1.0]
+            prob = LinearProblem(A, b)
+            linsolve = init(prob, alg)
+            sol = solve!(linsolve)
+            @test SciMLBase.successful_retcode(sol.retcode) || sol.retcode == ReturnCode.Default # The latter seems off...
+        end
+    end
+
+    @testset "Failure" begin
+        for alg in alglist
+            A = [1.0 1.0; 1.0 1.0]
+            b = [-1.0, 1.0]
+            prob = LinearProblem(A, b)
+            linsolve = init(prob, alg)
+            sol = solve!(linsolve)
+            @test !SciMLBase.successful_retcode(sol.retcode)
+        end
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,7 @@ const HAS_EXTENSIONS = isdefined(Base, :get_extension)
 if GROUP == "All" || GROUP == "Core"
     @time @safetestset "Quality Assurance" include("qa.jl")
     @time @safetestset "Basic Tests" include("basictests.jl")
+    @time @safetestset "Return codes" include("retcodes.jl")
     @time @safetestset "Re-solve" include("resolve.jl")
     @time @safetestset "Zero Initialization Tests" include("zeroinittests.jl")
     @time @safetestset "Non-Square Tests" include("nonsquare.jl")

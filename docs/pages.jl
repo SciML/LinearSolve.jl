3bff5868ad2dca4d8b638656816802235f438d29
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-02-05 15:20:34 -0800

    Add a page on choosing the right solver and architectural details
    This also documents the performance changes users will see with the new breaking.

--- docs/pages.jl
+++ docs/pages.jl
@@ -1,8 +1,9 @@
 # Put in a separate page so it can be used by SciMLDocs.jl
 
 pages = ["index.md",
-    "Tutorials" => Any["tutorials/linear.md"
-                       "tutorials/caching_interface.md"],
+    "Tutorials" => Any["tutorials/linear.md",
+                       "tutorials/caching_interface.md",
+                       "tutorials/accelerating_choices.md"],
     "Basics" => Any["basics/LinearProblem.md",
         "basics/common_solver_opts.md",
         "basics/OperatorAssumptions.md",

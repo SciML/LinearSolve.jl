bfd1467bbc0071af5d7f63e5a6b1df5e38d57e76
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-02-16 12:01:41 -0500

    Update LinearSolveSparseArraysExt.jl

--- ext/LinearSolveSparseArraysExt.jl
+++ ext/LinearSolveSparseArraysExt.jl
@@ -47,7 +47,7 @@ function LinearSolve.defaultalg(A::AbstractSparseMatrixCSC{Tv, Ti}, b,
         assump::OperatorAssumptions{Bool}) where {Tv, Ti}
     ext = Base.get_extension(LinearSolve, :LinearSolveSparspakExt)
     if assump.issq && ext !== nothing
-        LinearSolve.DefaultLinearSolver(DefaultAlgorithmChoice.SparspakFactorization)
+        LinearSolve.DefaultLinearSolver(LinearSolve.DefaultAlgorithmChoice.SparspakFactorization)
     elseif !assump.issq
         error("Generic number sparse factorization for non-square is not currently handled")
     elseif ext === nothing

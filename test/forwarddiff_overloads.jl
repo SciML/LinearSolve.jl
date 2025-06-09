f55639a559691c49805def5f6d11be99fff89166
jClugstor <jadonclugston@gmail.com>
2025-06-09 13:23:24 -0400

    add tests for iterative and u0

--- test/forwarddiff_overloads.jl
+++ test/forwarddiff_overloads.jl
@@ -13,18 +13,31 @@ A, b = h([ForwardDiff.Dual(5.0, 1.0, 0.0), ForwardDiff.Dual(5.0, 0.0, 1.0)])
 
 prob = LinearProblem(A, b)
 overload_x_p = solve(prob)
-original_x_p = A \ b
+backslash_x_p = A \ b
+krylov_overload_x_p = solve(prob, KrylovJL_GMRES())
+@test ≈(overload_x_p, backslash_x_p, rtol = 1e-9)
+@test ≈(krylov_overload_x_p, backslash_x_p, rtol = 1e-9)
 
-@test ≈(overload_x_p, original_x_p, rtol = 1e-9)
+krylov_prob = LinearProblem(A, b, u0 = rand(3))
+krylov_u0_sol = solve(krylov_prob, KrylovJL_GMRES())
+
+@test ≈(krylov_u0_sol, original_x_p, rtol = 1e-9)
+
 
 A, _ = h([ForwardDiff.Dual(5.0, 1.0, 0.0), ForwardDiff.Dual(5.0, 0.0, 1.0)])
+backslash_x_p = A \ [6.0, 10.0, 25.0]
 prob = LinearProblem(A, [6.0, 10.0, 25.0])
-@test ≈(solve(prob).u, A \ [6.0, 10.0, 25.0], rtol = 1e-9) 
+
+@test ≈(solve(prob).u, backslash_x_p, rtol = 1e-9) 
+@test ≈(solve(prob, KrylovJL_GMRES()).u, backslash_x_p, rtol = 1e-9)
 
 _, b = h([ForwardDiff.Dual(5.0, 1.0, 0.0), ForwardDiff.Dual(5.0, 0.0, 1.0)])
 A = [5.0 6.0 125.0; 15.0 10.0 21.0; 25.0 45.0 5.0]
+backslash_x_p = A \ b
 prob = LinearProblem(A, b)
-@test ≈(solve(prob).u, A \ b, rtol = 1e-9)
+
+@test ≈(solve(prob).u, backslash_x_p, rtol = 1e-9)
+@test ≈(solve(prob, KrylovJL_GMRES()).u, backslash_x_p, rtol = 1e-9)
 
 A, b = h([ForwardDiff.Dual(10.0, 1.0, 0.0), ForwardDiff.Dual(10.0, 0.0, 1.0)])
 
@@ -36,6 +49,6 @@ cache.A = new_A
 cache.b = new_b
 
 x_p = solve!(cache)
-other_x_p = new_A \ new_b
+backslash_x_p = new_A \ new_b
 
-@test ≈(x_p, other_x_p, rtol = 1e-9)
+@test ≈(x_p, backslash_x_p, rtol = 1e-9)

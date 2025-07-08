8a97efd042660ea1ac27da9c57efeb25a72cc19a
jClugstor <jadonclugston@gmail.com>
2025-07-08 13:43:57 -0400

    use SciMLVerbosity not SciMLBase

--- src/LinearSolve.jl
+++ src/LinearSolve.jl
@@ -14,10 +14,11 @@ using LinearAlgebra: LinearAlgebra, BlasInt, LU, Adjoint, BLAS, Bidiagonal, Bunc
                     cholesky, cholesky!, diagind, dot, inv, ldiv!, ldlt!, lu, lu!, mul!, norm,
                     qr, qr!, svd, svd!
 using LazyArrays: @~, BroadcastArray
-using SciMLBase: SciMLBase, LinearAliasSpecifier, LinearVerbosity, Verbosity, @SciMLMessage, LinearVerbosity, AbstractSciMLOperator,
+using SciMLBase: SciMLBase, LinearAliasSpecifier, AbstractSciMLOperator,
                  init, solve!, reinit!, solve, ReturnCode, LinearProblem
 using SciMLOperators: SciMLOperators, AbstractSciMLOperator, IdentityOperator, MatrixOperator,
                      has_ldiv!, issquare
+using SciMLVerbosity: Verbosity, @SciMLMessage, @match, AbstractVerbositySpecifier
 using Setfield: @set, @set!
 using UnPack: @unpack
 using DocStringExtensions: DocStringExtensions
@@ -149,6 +150,7 @@ const BLASELTYPES = Union{Float32, Float64, ComplexF32, ComplexF64}
 
 function defaultalg_symbol end
 
+include("verbosity.jl")
 include("generic_lufact.jl")
 include("common.jl")
 include("extension_algs.jl")

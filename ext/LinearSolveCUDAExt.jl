cb435d8d73333a04f4eb09588f8cc84dfbb00c1e
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-07-22 01:20:13 -0400

    Fix LinearSolveCUDAExt.jl
    Should fix gpu failure

--- ext/LinearSolveCUDAExt.jl
+++ ext/LinearSolveCUDAExt.jl
@@ -3,7 +3,7 @@ module LinearSolveCUDAExt
 using CUDA
 using LinearSolve: LinearSolve, is_cusparse, defaultalg, cudss_loaded, DefaultLinearSolver, 
                    DefaultAlgorithmChoice, ALREADY_WARNED_CUDSS, LinearCache, needs_concrete_A,
-                   error_no_cudss_lu, CUDSS_LOADED, init_cacheval
+                   error_no_cudss_lu, init_cacheval
 using LinearSolve.LinearAlgebra, LinearSolve.SciMLBase, LinearSolve.ArrayInterface
 using SciMLBase: AbstractSciMLOperator
 

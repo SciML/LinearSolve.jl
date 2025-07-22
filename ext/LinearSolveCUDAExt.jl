ace17c7e4cbbf9cc5cdb3eb05c38a2c273085e22
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-07-22 02:03:42 -0400

    Update LinearSolveCUDAExt.jl

--- ext/LinearSolveCUDAExt.jl
+++ ext/LinearSolveCUDAExt.jl
@@ -26,7 +26,7 @@ function LinearSolve.defaultalg(A::CUDA.CUSPARSE.CuSparseMatrixCSR{Tv, Ti}, b,
 end
 
 function LinearSolve.error_no_cudss_lu(A::CUDA.CUSPARSE.CuSparseMatrixCSR)
-    if !LinearSolve.cudss_loaded[]
+    if !LinearSolve.cudss_loaded(A)
         error("CUDSS.jl is required for LU Factorizations on CuSparseMatrixCSR. Please load this library.")
     end
     nothing

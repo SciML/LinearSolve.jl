592dc07fc3ad70fdddf064efa4c643e3b6913926
jClugstor <jadonclugston@gmail.com>
2025-07-28 14:27:21 -0400

    add test for forwarddiff static arrays

--- test/forwarddiff_overloads.jl
+++ test/forwarddiff_overloads.jl
@@ -193,3 +193,31 @@ overload_x_p = solve(prob, UMFPACKFactorization())
 backslash_x_p = A \ b
 
 @test ≈(overload_x_p, backslash_x_p, rtol = 1e-9)
+
+
+# Test StaticArrays
+# They don't go through the overloads
+# But we should test that the overloads don't mess anything up
+function static_h(p)
+    A = @SMatrix [p[1] p[2]+1 p[2]^3;
+                  3*p[1] p[1]+5 p[2] * p[1]-4;
+                  p[2]^2 9*p[1] p[2]]
+
+    b = SA[p[1] + 1, p[2] * 2, p[1]^2]
+
+    (A, b)
+end
+
+function static_linsolve(p)
+    A, b = static_h(p)
+    prob = LinearProblem(A, b)
+    solve(prob)
+end
+
+function static_backslash(p)
+    A, b = static_h(p)
+    A \ b
+end
+
+@test (ForwardDiff.jacobian(static_linsolve, [5.0, 5.0]) ≈
+       ForwardDiff.jacobian(static_backslash, [5.0, 5.0]))

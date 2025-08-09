910a2e5055954bb4bd83f324a8b8b6170e44f2c8
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-08-09 16:20:49 -0400

    Update test/nopre/jet.jl

--- test/nopre/jet.jl
+++ test/nopre/jet.jl
@@ -33,8 +33,8 @@ prob_sparse_spd = LinearProblem(A_sparse_spd, b)
     JET.@test_opt solve(prob, NormalBunchKaufmanFactorization())
     
     # CholeskyFactorization and SVDFactorization now pass JET tests
-    JET.@test_opt solve(prob_spd, CholeskyFactorization())
-    JET.@test_opt solve(prob, SVDFactorization())
+    JET.@test_opt solve(prob_spd, CholeskyFactorization()) broken=true
+    JET.@test_opt solve(prob, SVDFactorization()) broken=true
     
     # Tests with known type stability issues - marked as broken
     JET.@test_opt solve(prob, QRFactorization()) broken=true

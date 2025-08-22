de311638b6f1d5c326f4b930c7ab7e0671708979
jClugstor <jadonclugston@gmail.com>
2025-08-22 12:43:51 -0400

    from Bool to LinearVerbosity

--- ext/LinearSolveRecursiveFactorizationExt.jl
+++ ext/LinearSolveRecursiveFactorizationExt.jl
@@ -42,7 +42,7 @@ const PREALLOCATED_RF32_LU = begin
 end
 
 function LinearSolve.init_cacheval(alg::RF32MixedLUFactorization{P, T}, A, b, u, Pl, Pr,
-        maxiters::Int, abstol, reltol, verbose::Bool,
+        maxiters::Int, abstol, reltol, verbose::LinearVerbosity,
         assumptions::LinearSolve.OperatorAssumptions) where {P, T}
     # Pre-allocate appropriate 32-bit arrays based on input type
     m, n = size(A)

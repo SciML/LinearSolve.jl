23398ef5028bfe6e8b0726ab7d74dc9bbe4c74b6
jClugstor <jadonclugston@gmail.com>
2025-09-21 15:33:11 -0400

    fix aqua test

--- src/common.jl
+++ src/common.jl
@@ -332,7 +332,7 @@ function __init(prob::LinearProblem, alg::SciMLLinearSolveAlgorithm,
         else
             verbose_spec = LinearVerbosity{false}()
         end
-    elseif verbose isa Verbosity.Type
+    elseif verbose isa SciMLLogging.Verbosity.Type
         verbose_spec = LinearVerbosity(verbose)
     else
         verbose_spec = verbose

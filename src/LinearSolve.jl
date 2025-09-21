03f26294c4172358402a031fbce83550aa623d3e
jClugstor <jadonclugston@gmail.com>
2025-09-21 15:31:55 -0400

    remove incorrect exports

--- src/LinearSolve.jl
+++ src/LinearSolve.jl
@@ -503,7 +503,6 @@ export OperatorAssumptions, OperatorCondition
 
 export LinearSolveAdjoint
 
-export LinearVerbosity, LinearErrorControlVerbosity, LinearPerformanceVerbosity,
-       LinearNumericalVerbosity
+export LinearVerbosity
 
 end

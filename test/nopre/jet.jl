b2e426cadfdc82d704cdee038af0b2cee357425b
jClugstor <jadonclugston@gmail.com>
2025-10-06 18:05:22 -0400

    set tests broken only for < 1.11

--- test/nopre/jet.jl
+++ test/nopre/jet.jl
@@ -146,6 +146,11 @@ end
     # Test for Default alg choosing for DualLinear Problems
     # These should both produce a LinearCache
     alg = LinearSolve.DefaultLinearSolver(LinearSolve.DefaultAlgorithmChoice.GenericLUFactorization)
-    JET.@test_opt init(dual_prob, alg) 
-    JET.@test_opt init(dual_prob) 
+    if VERSION < v"1.11"
+        JET.@test_opt init(dual_prob, alg) broken=true
+        JET.@test_opt init(dual_prob) broken=true
+    else
+        JET.@test_opt init(dual_prob, alg)
+        JET.@test_opt init(dual_prob)
+    end
 end

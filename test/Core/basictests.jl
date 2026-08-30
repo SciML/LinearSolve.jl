271a3f8233f2b0a43e0a58a8a2434801e4dcd47c
Christopher Rackauckas <accounts@chrisrackauckas.com>
2026-08-30 00:02:26 +0000

    Apply suggestion from @ChrisRackauckas

--- test/Core/basictests.jl
+++ test/Core/basictests.jl
@@ -882,8 +882,6 @@ end
     end
 
     @testset "Preconditioners" begin
-        @test Base.isexported(LinearSolve, :InvPreconditioner)
-
         @testset "Vector Diagonal Preconditioner" begin
             x = rand(n, n)
             y = rand(n, n)
